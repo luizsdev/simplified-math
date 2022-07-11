4fe2bbbdf864ff77335a82c784d9a05adc1f1f4a
Nicholas Costa <nicholascostadev@gmail.com>
2022-07-11 18:13:15 -0300

    Update method

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export const pi = ()=>{
-    console.log("3,14")
-}
+  /**
+   *
+   * @param decimals Amount of decimals after the comma, `defaults` to 2
+   *
+   */
+  export const pi = (decimals = 2) => {
+    if (decimals) return Math.PI.toFixed(decimals);
+
+    return Math.PI;
+  };
